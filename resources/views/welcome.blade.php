be7dfde3cd9638773f897e650fca8e24d1c868ba
Vinícius Furtado Pecci de Lima <viniciuspecci@hotmail.com>
2022-01-17 02:10:35 -0300

    Estilização do layout e correções no model, view e controller

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,28 @@
 @section('title', 'CRUD Laravel')
 @section('content')
 
-@foreach($registers as $register)
-    <p>{{ $register->name }} -- {{ $register->email }}</p>
-@endforeach
+<div id="search-container" class="col-md-12">
+    <h1>Busque um registro</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+    </form>
+</div>
+<div id="registers-container" class="col-md-12">
+    <h2>Usuários cadastrados:</h2>
+    <div id="cards-container" class="row">
+        @foreach ($registers as $register)
+            <div class="card col-md-3">
+                <img src="/img/register_placeholder3.jpg" alt="{{ $register->name }}">
+                <div class="card-body">
+                    <p class="card-date">{{ $register->state }} - {{ $register->city }}</p>
+                    <h5 class="card-title">{{ $register->name }}</h5>
+                    <p class="card-participants">{{ $register->email }}</p>
+                    <a href="#" class="btn btn-primary">Visualizar</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
 
 @endsection
 
